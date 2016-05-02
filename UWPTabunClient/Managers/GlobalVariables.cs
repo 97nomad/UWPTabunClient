0887a4adaa5ecea255e3b9e3ffd8af1a4fae1748
97nomad <97nomad@gmail.com>
2016-05-02 11:13:51 +0300

    Упс. Опечатка.

--- UWPTabunClient/Managers/GlobalVariables.cs
+++ UWPTabunClient/Managers/GlobalVariables.cs
@@ -11,7 +11,7 @@ namespace UWPTabunClient.Managers
         public static string linkRoot = "https://tabun.everypony.ru";
         public static string linkBlog = "https://tabun.everypony.ru/blog/";
         public static string linkMainpage = "https://tabun.everypony.ru/index/";
-        public static string linkProfile = "https://tabun/everypony.ru/profile/";
+        public static string linkProfile = "https://tabun.everypony.ru/profile/";
 
         public static string linkAjaxStreamComments = "https://tabun.everypony.ru/ajax/stream/comments/";
         public static string linkAjaxStreamTopics = "https://tabun.everypony.ru/ajax/stream/topic/";
